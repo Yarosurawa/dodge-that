9bd8d52050908d93816c31bea1404e2fe38cb9d2
Yarosurawa <yarosurawa@gmail.com>
2024-04-18 22:59:50 +0300

    Some stuff

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+if (window.innerWidth > 1100) {
+    var circleRadius = 25;
+    var enemyRadius = 10;
+    var acceleration = 1;
+} else {
+    var circleRadius = 12.5;
+    var enemyRadius = 5;
+    var acceleration = 0.5;
+    
+}
+
 let player = {
     elem: document.getElementById('player'),
-    x: window.innerWidth/2,
-    xc: window.innerWidth/2 + 25,
-    y: window.innerHeight/2,
-    yc: window.innerHeight/2 + 25,
+    x: window.innerWidth/2 - circleRadius,
+    xc: window.innerWidth/2,
+    y: window.innerHeight/2 - circleRadius,
+    yc: window.innerHeight/2,
     vx: 0,
     vy: 5,
     pressingLeft: false,
@@ -23,9 +34,9 @@ let enemies = [];
 class Enemy {
     constructor(x, y, life){
         this.x = x,
-        this.xc = x + 10,
+        this.xc = x + enemyRadius,
         this.y = y,
-        this.yc = y + 10,
+        this.yc = y + enemyRadius,
         this.life = life
     }
 }
@@ -33,9 +44,9 @@ class Enemy {
 let uranium = {
     elem: document.getElementById('uranium'),
     x: window.innerWidth/2,
-    xc: this.x + 25,
+    xc: this.x + enemyRadius,
     y: window.innerHeight/2,
-    yc: this.y + 25,
+    yc: this.y + enemyRadius,
 }
 
 function phisFrame(){
@@ -46,20 +57,20 @@ function phisFrame(){
 
             player.vy -= 0.1
             if (player.pressingLeft && player.pressingRight) {} else if (player.pressingLeft && player.vx > -5) {
-                player.vx -= 0.25
+                player.vx -= 0.25 * acceleration
             } else if (player.pressingRight && player.vx < 5) {
-                player.vx += 0.25
+                player.vx += 0.25 * acceleration
             }
             player.x += player.vx
             player.y -= player.vy
-            player.xc = player.x + 25
-            player.yc = player.y + 25
+            player.xc = player.x + circleRadius
+            player.yc = player.y + circleRadius
             player.elem.style.left = player.x + "px"
             player.elem.style.top = player.y + "px"
 
             enemies.forEach((enem)=>{
                 enem.life += 1
-                if (getDistance(enem.xc, player.xc, enem.yc, player.yc) < 35) {
+                if (getDistance(enem.xc, player.xc, enem.yc, player.yc) < 35 * acceleration) {
                     if (enem.life > 120) {
                         gameOver()
                     }
@@ -69,7 +80,7 @@ function phisFrame(){
                 gameOver()
             )
 
-            if( getDistance(uranium.xc, player.xc, uranium.yc, player.yc) < 35) {
+            if( getDistance(uranium.xc, player.xc, uranium.yc, player.yc) < 35 * acceleration) {
                 moveUranium()
                 createEnemy(Math.floor((Math.random()*(window.innerWidth - 30)+1) / 20) * 20, Math.floor((Math.random()*(window.innerHeight - 30)+1) / 20) * 20)
                 score++
@@ -86,7 +97,7 @@ function gameStart() {
     phisFrame()
 
     addEventListener("keydown", (e)=>{
-        if (e.code == "KeyW" || e.code == "Space" || e.code == "ArrowUp" ) {   player.vy += 5   }
+        if (e.code == "KeyW" || e.code == "Space" || e.code == "ArrowUp" ) {   player.vy += 5  * acceleration }
         else if (e.code == "KeyA" || e.code == "ArrowLeft") {    player.pressingLeft = true;    }
         else if (e.code == "KeyD" || e.code == "ArrowRight") {   player.pressingRight = true;   }
         else if (e.code == "Escape") {   gameOver()                     }
@@ -97,7 +108,10 @@ function gameStart() {
         else if (e.code == "KeyD" || e.code == "ArrowRight") {   player.pressingRight = false;   }
     })
 
-    
+    document.getElementById('leftbtn').addEventListener("touchstart", ()=> {player.pressingLeft = true})
+    document.getElementById('rightbtn').addEventListener("touchstart", ()=> {player.pressingRight = true})
+    document.getElementById('leftbtn').addEventListener("touchend", ()=> {player.pressingLeft = false})
+    document.getElementById('rightbtn').addEventListener("touchend", ()=> {player.pressingRight = false})    
 }
 
 function createEnemy(x, y) {
@@ -112,8 +126,9 @@ function createEnemy(x, y) {
 
 function gameOver() {
     document.getElementById("gameOverText").style.transition = "transform 1s ease"
-    document.getElementById("gameOverText").style.transform = "translateX(calc(100% - 100px))"
+    document.getElementById("gameOverText").style.transform = "translateX(0)"
     gameStopped = 1
+    setTimeout(()=>{location.reload()}, 4000)
 }
 
 addEventListener("keydown", ()=>{
@@ -121,6 +136,7 @@ addEventListener("keydown", ()=>{
         gameStarted = 1
         gameStart()
         document.getElementById('score').style.color = "#777"
+        document.getElementById('buttons').style.opacity = "0"
         setTimeout(()=>{
             moveUranium()
         }, 2000)
@@ -145,8 +161,8 @@ function moveUranium() {
         uranium.x = randomW
         uranium.y = randomH
     }
-    uranium.xc = uranium.x + 10
-    uranium.yc = uranium.y + 10
+    uranium.xc = uranium.x + enemyRadius
+    uranium.yc = uranium.y + enemyRadius
     uranium.elem.style.left = uranium.x + "px"
     uranium.elem.style.top = uranium.y + "px"
 }
@@ -157,4 +173,17 @@ function getDistance(x1, x2, y1, y2) {
     let distance = (a + b) ** 0.5
     return distance
 }
+document.getElementById("jumpbtn").addEventListener("touchstart", ()=>{
+    if (!gameStarted) {
+        gameStarted = 1
+        gameStart()
+        document.getElementById('score').style.color = "#777"
+        document.getElementById('buttons').style.opacity = "0"
+        setTimeout(()=>{
+            moveUranium()
+        }, 2000)
+    } else {
+        player.vy += 5 * acceleration
+    }
+})
 
